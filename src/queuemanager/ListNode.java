8491ddee7a61e2146127e39e4282e179f07fd291
KneeHigh80 <lukasrichly@gmail.com>
2024-03-05 14:54:58 +0000

    Additions to ListNode
    I had to expand the capabilities of the ListNode because when inserting a new Node I need to be able to set the next and previous attribute from maybe 3 instances of Node

--- src/queuemanager/ListNode.java
+++ src/queuemanager/ListNode.java
@@ -7,11 +7,13 @@ package queuemanager;
 public class ListNode<T> {
     private final PriorityItem<T> item;
     private ListNode<T> next;
+    private ListNode<T> previous;
     
     //Constructor
-    public ListNode(PriorityItem<T> item, ListNode<T> next) {
+    public ListNode(PriorityItem<T> item, ListNode<T> next, ListNode<T> previous) {
         this.item = item;
         this.next = next;
+        this.previous = previous;
     }
     
     //Getters
@@ -27,6 +29,19 @@ public class ListNode<T> {
     public ListNode<T> getNext() {
         return next;
     }
+    
+    public ListNode<T> getPrevious() {
+        return previous;
+    }
+
+    public void setNext(ListNode<T> next) {
+        this.next = next;
+    }
+
+    public void setPrevious(ListNode<T> previous) {
+        this.previous = previous;
+    }
+    
     
     
 }
--- src/queuemanager/orderedLinkedListPriorityQueue.java
+++ src/queuemanager/orderedLinkedListPriorityQueue.java
@@ -4,23 +4,43 @@ package queuemanager;
 /**
  *
  * @author lukas
+ * @param <T>
  */
 public class OrderedLinkedListPriorityQueue<T> implements PriorityQueue<T>  {
    
     private ListNode<T> top;
+    private ListNode<T> tail;
     
     public OrderedLinkedListPriorityQueue() {
         top = null;
-    }
-
-    @Override
-    public void add(T item, int priority) throws QueueOverflowException {
+        tail = null;
         
     }
 
     @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+        PriorityItem<T> new_item = new PriorityItem(item, priority);
+        ListNode<T> new_Node = new ListNode(new_item, null);
+        
+        
+        if(top == null) {
+            top = tail = new_Node;
+        }
+        ListNode<T> current = top;
+        while (current.getNext() != null && current.getItemPriority() < new_Node.getItemPriority()) {
+            current = current.getNext();
+        }
+        new_Node.next = current.getNext();
+        current.next = new_Node;
+    }
+
+    @Override
     public T head() throws QueueUnderflowException {
-        return 
+        if(isEmpty()){
+            throw new QueueUnderflowException();
+        }
+        return top.getListItem();
+        
     }
 
     @Override
@@ -30,6 +50,6 @@ public class OrderedLinkedListPriorityQueue<T> implements PriorityQueue<T>  {
 
     @Override
     public boolean isEmpty() {
-        
+        return top == null;
     }
 }
